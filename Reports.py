acbfcf9ba8a3600a1adbb5f0c3c276b1c22872b5
usercontacte <88674283+usercontacte@users.noreply.github.com>
2021-08-25 20:55:29 +0800

    Update Reports.py

--- Reports.py
+++ Reports.py
@@ -13,10 +13,9 @@ class Class_Reports:
     st.title('Web Scrapping Reports')
 
     # @st.cache(allow_output_mutation=True, hash_funcs={"_thread.RLock": lambda _: None})
-    # DBConnection = ""
     def __init__(self):
         self.conn = self.con_init()
-        # DBConnection = self.conn
+        self.display_results()
         self.main()
 
     # @st.cache(allow_output_mutation=True)
@@ -28,6 +27,36 @@ class Class_Reports:
         with self.conn.cursor() as cur:
             cur.execute(query)
             return cur.fetchall()
+        
+     def count_forsale(self, d):
+        result = self.query(f"SELECT COUNT(IF(status = 'for_sale', 1, NULL)) 'For Sale' FROM hemnet_bostad_salda WHERE date_created = '{d}';")
+        return result[0][0]
+
+    def count_sold(self, d):
+        result = self.query(f"SELECT COUNT(IF(status = 'sold', 1, NULL)) 'Sold' FROM hemnet_bostad_salda WHERE sold_date = '{d}';")
+        return result[0][0]
+
+    def count_private(self, d):
+        result = self.query(f"SELECT COUNT(IF(status = 'private', 1, NULL)) 'Private' FROM hemnet_bostad_salda WHERE date_updated = '{d}';")
+        return result[0][0]
+    
+    def forsale(self):
+        result = self.query(f"SELECT COUNT(IF(status = 'for_sale', 1, NULL)) 'For Sale' FROM hemnet_bostad_salda;")
+        return result[0][0]
+
+    def sold(self):
+        result = self.query(f"SELECT COUNT(IF(status = 'sold', 1, NULL)) 'Sold' FROM hemnet_bostad_salda;")
+        return result[0][0]
+
+    def private(self):
+        result = self.query(f"SELECT COUNT(IF(status = 'private', 1, NULL)) 'Private' FROM hemnet_bostad_salda;")
+        return result[0][0]
+    
+    def display_results(self):
+        st.header('Total Records')
+        st.write(f'For Sale: {self.forsale()}')
+        st.write(f'Sold: {self.sold()}')
+        st.write(f'Private: {self.private()}')
 
     def main(self):
 
@@ -102,23 +131,7 @@ class Class_Reports:
         # st.line_chart(df
         st.area_chart(df)
         st.balloons()
-        
-#         chart_data = pd.DataFrame(np.random.randn(20, 3), columns=['a', 'b', 'c'])
-#         st.area_chart(chart_data)
-
-    def count_forsale(self, d):
-        result = self.query(f"SELECT COUNT(IF(status = 'for_sale', 1, NULL)) 'For Sale' FROM hemnet_bostad_salda WHERE date_created = '{d}';")
-        return result[0][0]
-
-    def count_sold(self, d):
-        result = self.query(f"SELECT COUNT(IF(status = 'sold', 1, NULL)) 'Sold' FROM hemnet_bostad_salda WHERE sold_date = '{d}';")
-        return result[0][0]
-
-    def count_private(self, d):
-        result = self.query(f"SELECT COUNT(IF(status = 'private', 1, NULL)) 'Private' FROM hemnet_bostad_salda WHERE date_updated = '{d}';")
-        return result[0][0]
-
-
+ 
 if __name__ == '__main__':
     try:
         Class_Reports()
